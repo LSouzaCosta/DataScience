129379d596d5435610b43bae672a833f193ed093
Lucas_DS <lucasabadini@hotmail.com>
2024-11-21 22:46:50 -0300

    Últimas modificações e correções do problema do Titanic.

--- Titanic/PreProcessing_titanic.py
+++ Titanic/PreProcessing_titanic.py
@@ -8,17 +8,16 @@ import seaborn as sns
 
 def PP_titanic_function(df_model):
     # Executando uma limpeza básica dos dados, excluindo as colunas que não auxiliam na predição:
-    df_model.drop(columns=["PassengerId", "Name", "Ticket"], inplace= True)
+    df_model.drop(columns=["PassengerId", "Name", "Ticket"], inplace=True)
 
     # Tratando valores nulos com base no dtype da coluna:
-    # Criando um dicionário para preencher os valores de acordo com o tipo da coluna:
-    fill_values = {col: -1 if df_model[col].dtype in ['int64', 'float64'] else 'NA' for col in df_model.columns}
-    
-    # Usando fillna() com o dicionário
-    df_model.fillna(value=fill_values, inplace=True)
-    """df_model["Age"] = df_model["Age"].fillna(-1)
-    df_model["Cabin"] = df_model["Cabin"].fillna('NA')
-    df_model["Embarked"] = df_model["Embarked"].fillna("NA")"""
+    for col in df_model.columns:
+        if pd.api.types.is_numeric_dtype(df_model[col]):
+            # Preencher nulos com -1 se a coluna for numérica
+            df_model[col].fillna(-1, inplace=True)
+        else:
+            # Preencher nulos com 'NA' se a coluna não for numérica
+            df_model[col].fillna("NA", inplace=True)
 
     # Criando uma nova feature
     df_model["Taxa_x_SibSp"] = df_model["Fare"] * df_model["SibSp"]
